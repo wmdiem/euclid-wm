16d088d8f40443bfe8bd5f636017cf81641806f5
wmdiem <wmdiem@520abda2-7532-33de-3ab3-e9d792c13b5d>
2014-02-16 17:10:08 +0000

    added working handlers

--- trunk/handlers/default.sh
+++ trunk/handlers/default.sh
@@ -1,11 +1,27 @@
-if [ $1 ]  
+#!/bin/bash
+
+
+while read line
+
+do
+if [ "$line" = "" ];
+ 
+then 
+	echo -n $PATH | xargs -d : -I {} find {} -maxdepth 1 -executable -printf '%P\n' | sort -u 
+	#ls /usr/bin | sort
+	echo -e "\n"
+
+elif [[ $line == exe* ]];
 
 then 
+	exec ${line:5:${#line}-5};
 
-echo -n $PATH | xargs -d : -I {} find {} -maxdepth 1 -executable -printf '%P\n' | sort -u | grep -i $1
- 
+
 else
 
-echo -n $PATH | xargs -d : -I {} find {} -maxdepth 1 -executable -printf '%P\n' | sort -u 
+	#ls /usr/bin | grep "$line"
+	echo -n $PATH | xargs -d : -I {} find {} -maxdepth 1 -executable -printf '%P\n' |  sort -u | grep -i $line
+	echo -e "\n"
 
 fi
+done 
